836530897d79fb3276409b53be08c3187e262421
root <root@DESKTOP-R9SOOB8>
2024-01-12 11:00:28 +0100

    validator

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ int main() {
     std::cout << " ░ ░        ░           ░  ░      ░ ░   ░  ░  ░       ░  ░     ░  ░         ░       ░ " << std::endl;
     std::cout << " ░ ░                                                                                  " << std::endl;
 
-    FILE* fp = fopen("test/if.ygo", "r");
+    FILE* fp = fopen("test/test.ygo", "r");
     if (!fp) {
         std::cerr << "Error opening file" << std::endl;
         return 1;
